9d6afda4514d035304698de3382db3917d8ef1c1
Carchedi <carchedi.ufjf@gmail.com>
2025-11-29 22:12:43 -0300

    Os endpoints para criar um novo pedido e listar todos os pedidos

--- index.js
+++ index.js
@@ -12,6 +12,34 @@ app.get("/", (req, res) => {
   res.send("Primeiro GET funcionando");
 });
 
+// Criar um novo pedido
+app.post("/order", (req, res) => {
+  const { cliente, produto, valor } = req.body;
+
+  if (!cliente || !produto || !valor) {
+    return res.status(400).send({
+      message: "Os campos 'cliente', 'produto' e 'valor' são obrigatórios.",
+    });
+  }
+
+  const newOrder = {
+    id: `v${Date.now()}`, // Gerando um ID único simples
+    cliente,
+    produto,
+    valor,
+    entregue: false,
+    timestamp: new Date(),
+  };
+
+  orders.push(newOrder);
+  res.status(201).send(newOrder);
+});
+
+// Listar todos os pedidos
+app.get("/order/list", (req, res) => {
+  res.status(200).send(orders);
+});
+
 app.listen(port, () => {
   console.log(`Exemplo de app escutando na porta ${port}`);
 });
